309fa63d738badac81fe8cca005d2c1b63c3caec
Abhishek-Alli <alliabhishek138@gmail.com>
2026-09-06 14:50:28 +0530

    fix: add class-variance-authority dep; fix implicit any in sidebar onClick

--- apps/web/components/ui/sidebar.tsx
+++ apps/web/components/ui/sidebar.tsx
@@ -265,7 +265,7 @@ function SidebarTrigger({
       variant="ghost"
       size="icon-sm"
       className={cn(className)}
-      onClick={(event) => {
+      onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
         onClick?.(event)
         toggleSidebar()
       }}
